11ee6100f8b0f8656a12797122851e96b505437b
Frederik Reich <frederikreich@googlemail.com>
2014-01-22 00:29:33 +0100

    Added AutoRotate.

--- Actions/AutoRotate.cs
+++ Actions/AutoRotate.cs
@@ -4,40 +4,37 @@ using System.Collections;
 namespace HutongGames.PlayMaker.Actions
 {
 	[ActionCategory("Custom")]
-	[Tooltip("Wiggles around on a sinus axis.")]
-	public class SinusWiggle : FsmStateAction
+	[Tooltip("Constantly rotates a GameObject around an axis.")]
+	public class AutoRotate : FsmStateAction
 	{
+
 		public FsmOwnerDefault gameObject;
-		public FsmBool animate;
-		public FsmFloat multiplier;
-		public FsmFloat timeFactor;
+		public FsmVector3 rotation;
 		public bool everyFrame;
 		
 		public override void Reset()
 		{
 			gameObject = null;
-			animate = false;
-			multiplier = 5f;
-			timeFactor = 0.5f;
+			rotation = null;
 			everyFrame = false;
 		}
-
+		
 		public override void OnEnter()
 		{
-			DoWiggle();
+			DoRotate();
 			
 			if (!everyFrame)
 			{
 				Finish();
 			}		
 		}
-		
+
 		public override void OnUpdate()
 		{
-			DoWiggle();
+			DoRotate();
 		}
 
-		void DoWiggle()
+		void DoRotate()
 		{
 			var go = Fsm.GetOwnerDefaultTarget(gameObject);
 			if (go == null)
@@ -47,10 +44,7 @@ namespace HutongGames.PlayMaker.Actions
 			
 			if (gameObject!=null)
 			{
-				// content.Value = textAsset.text;
-				//	UnityEngine.Debug.Log(content.Value);
-				
-				go.transform.eulerAngles = new Vector3(0f, Mathf.Sin(Time.time * timeFactor.Value) * multiplier.Value, 0f);	
+				go.transform.Rotate(rotation.Value * Time.deltaTime, Space.World);
 			}
 		}
 	}
--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 A collection of playmaker actions i created, after installation you can find them under the "Custom" tab in the playmaker action browser.
 
 ## List of Actions
-- SinusWiggle - A basic camera wiggle for presentations, based on the wiggle script used in the demo of [Dynamic Circles](https://www.assetstore.unity3d.com/#/content/800) by [Blackish](https://www.assetstore.unity3d.com/#/publisher/3).
+- SinusWiggle: A basic camera wiggle for presentations, based on the wiggle script used in the demo of [Dynamic Circles](https://www.assetstore.unity3d.com/#/content/800) by [Blackish](https://www.assetstore.unity3d.com/#/publisher/3).
+- AutoRotate: Rotates a GameObject around its axis constantly.
 
 ## License
 Copyright (c) 2013 F. Rick Reich. Licensed under the terms of the MIT license.
